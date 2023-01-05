e2e9ba6de2cfb427c671f82e3295c2c028623122
lesfgoh <107750978+lesfgoh@users.noreply.github.com>
2023-01-05 17:11:03 +0800

    Update latestbot.py
    Just added a bit of new stuff.

--- latestbot.py
+++ latestbot.py
@@ -1,7 +1,3 @@
-
-
-
-
 from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 from requests import *
@@ -9,9 +5,13 @@ from requests import *
 import logging
 
 
+addresses = dict()
 
 
-API_KEY = 'token'
+
+
+
+API_KEY = '1739361402:AAHjOM-pKQRuA25ZB2b5iAIkXA391CVCftE'
 
 
 
@@ -23,7 +23,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
+# Define a few command handlers. These take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 # Best practice would be to replace context with an underscore,
 # since context is an unused local variable.
@@ -32,6 +32,40 @@ logger = logging.getLogger(__name__)
 def start(update: Update, context: CallbackContext) -> None:
     """Sends explanation on how to use the bot."""
     update.message.reply_text('Hi! I\'m a bot run by Les F Goh. Click /info to learn more.', disable_notification = True)
+
+def add_address(chat_ID, location):
+    addresses[chat_ID] = location.lower()
+
+def notify(location):
+    for i in addresses:
+        if location.lower() in addresses[i]:
+            print(i)
+
+def submit(update: Update, context: CallbackContext) -> None:
+    location = context.args[0].lower()
+    for i in addresses:
+        if location in addresses[i]:
+            update.message.reply_text(i, disable_notification = True)
+            context.bot.send_message(chat_id=str(i), text='A ride has opened up to ' + location + ' !')
+    
+    
+def delete(chat_ID):
+    del(addresses[chat_ID])
+
+def init(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(context.args)
+    if context.args:
+        add_address(update.effective_user.id, context.args[0])
+    else:
+        update.message.reply_text("Please provide locations which you live near, for example: /sethome CCK. Spaces are not allowed!", disable_notification = True)
+
+def show(update:Update, context: CallbackContext) -> None:
+    update.message.reply_text("Show function is being called.")
+    update.message.reply_text(addresses)
+    
+def showme(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Showme function is being called.")
+    update.message.reply_text(addresses[update.effective_chat.id])
 
 def info (update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Please enter /sethome <General Area> e.g /sethome CCK . For now, the bot doesn't accept spaces: Input YewTee instead of Yew Tee. Any input will be changed to lowercase letters! \nYou can either message me directly, or in the GoBek group. For more questions, please contact my creator at @importr on.", disable_notification = True)
@@ -68,7 +102,7 @@ def match(update:Update, context: CallbackContext):
         pass
 
 def feedback(update: Update, context: CallbackContext):
-    update.message.reply_text("Please directly message @importr, my creator, to suggest new functionalities or even just to criticise their grammar. Any reason to procrastinate on their tutorials is much appreciated!", disable_notification = True)
+    update.message.reply_text("Please directly message @importr, my creator, to suggest new functionalities or even just to criticise their grammar!", disable_notification = True)
 
 
 def main() -> None:
@@ -90,6 +124,13 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("sethome", sethome))
     dispatcher.add_handler(CommandHandler("match", match))
     dispatcher.add_handler(CommandHandler("feedback", feedback))
+    
+    # Only these commands are being tested now. The rest are not relevant, and are for different purposes.
+    
+    dispatcher.add_handler(CommandHandler("show", show))
+    dispatcher.add_handler(CommandHandler("showme", showme))
+    dispatcher.add_handler(CommandHandler("init", init))
+    dispatcher.add_handler(CommandHandler("submit", submit))
 
 
 ##    dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))
@@ -105,12 +146,6 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
 
 
 
